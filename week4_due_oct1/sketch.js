f79a7bddef9e7c6c8109e12bb391da7b9725c8f3
kristitang-dev <kristitang1@gmail.com>
2026-09-24 19:06:52 -0400

    downloaded week4's updates

---
+++ week4_due_oct1/sketch.js
@@ -0,0 +1,32 @@
+// This is the sketch.js file.
+// Press 's' to export the SVG.
+// Note that p5.js is used in 'global mode'. 
+
+p5.disableFriendlyErrors = true; // keep warnings quiet
+let bDoExportSvg = false; 
+
+function setup(){
+  // These canvas dimensions are 8.5"x11" at 96 dpi
+  createCanvas(816, 1056); 
+}
+
+function keyPressed(){
+  if (key == 's'){ 
+    bDoExportSvg = true; 
+  }
+}
+
+function draw(){
+  background(255); 
+  if (bDoExportSvg){
+    beginRecordSvg("myOutput.svg");
+  }
+
+  // Draw stuff here, such as:
+  line(0,0, mouseX, mouseY); 
+
+  if (bDoExportSvg){
+    endRecordSvg();
+    bDoExportSvg = false;
+  }
+}
