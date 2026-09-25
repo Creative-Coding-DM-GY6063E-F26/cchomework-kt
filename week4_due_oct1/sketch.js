44e7b19d189969244db1cfd6a1c47b69c65a7687
kristitang-dev <kristitang1@gmail.com>
2026-09-24 20:47:05 -0400

    Sep24 class update

--- week4_due_oct1/sketch.js
+++ week4_due_oct1/sketch.js
@@ -8,9 +8,15 @@ let bDoExportSvg = false;
 function setup(){
   // These canvas dimensions are 8.5"x11" at 96 dpi
   createCanvas(816, 1056); 
+
+  //randomSeed(100); // random seed for randomness
+  //noLoop(); // don't loop, just draw once
 }
 
 function keyPressed(){
+  //if (key == 'r'){
+  //  draw(); // redraw the canvas
+  //}
   if (key == 's'){ 
     bDoExportSvg = true; 
   }
@@ -18,12 +24,25 @@ function keyPressed(){
 
 function draw(){
   background(255); 
+
   if (bDoExportSvg){
     beginRecordSvg("myOutput.svg");
   }
 
-  // Draw stuff here, such as:
-  line(0,0, mouseX, mouseY); 
+  //Draw stuff here, such as:
+  //line(0,0, mouseX, mouseY); 
+  for(let j=0; j<height; j+=5){
+    for(let i = 0; i < width; i+=5){
+      circle(i, j, 5);
+    }
+  }
+  translate(random(10),2)
+  for(let j=0; j<height; j+=5){
+    for(let i = 0; i < width; i+=5){
+      circle(i, j, 5);
+    }
+  }
+
 
   if (bDoExportSvg){
     endRecordSvg();
